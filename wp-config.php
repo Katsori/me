07a61f6b6ffb1d19abbb57a3b9b77d7828f7210e
Katsori <podpiscikp160@gmail.com>
2024-02-02 18:06:35 +0200

    Ready to publish

--- wp-config.php
+++ wp-config.php
@@ -83,11 +83,8 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-define( 'WP_HOME','http://localhost:8080/me/' );
-define( 'WP_SITEURL','http://localhost:8080/me/' );
-
-define('FORCE_SSL_LOGIN',false);
-define('FORCE_SSL_ADMIN',false);
+define( 'WP_HOME','http://localhost:8080/me' );
+define( 'WP_SITEURL','http://localhost:8080/me' );
 
 /* That's all, stop editing! Happy publishing. */
 
