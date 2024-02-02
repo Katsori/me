d748661cda0d6040c5bbdd2dbb717ec256fdc704
Katsori <podpiscikp160@gmail.com>
2024-02-02 17:49:34 +0200

    Ready to publish

--- wp-config.php
+++ wp-config.php
@@ -83,8 +83,8 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-define( 'WP_HOME','http://localhost:8080/wordpress' );
-define( 'WP_SITEURL','http://localhost:8080/wordpress' );
+define( 'WP_HOME','https://katsori.github.io/me/' );
+define( 'WP_SITEURL','https://katsori.github.io/me/' );
 
 define('FORCE_SSL_LOGIN',false);
 define('FORCE_SSL_ADMIN',false);
